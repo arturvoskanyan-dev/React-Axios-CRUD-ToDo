72be0d687663231cf9f54f71663d747a71adeb05
arturvoskanyan-dev <arturvoskanyan.dev@gmail.com>
2025-03-26 14:03:03 +0400

    style : Create header desing with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Header} from "./components/index"
+import './App.css'
+
+function App() {
+
+  return (
+    <section className='p-2.5 w-[400px] bg-dark-brown rounded-md shadow-2xl'>
+      <Header />
+    </section>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <article>
+                <h1 className='text-white text-3xl font-black'>ToDo List</h1>
+            </article>
+            <section className='flex'>
+                <input
+                    type="text"
+                    className='p-1.5 mr-4 w-full text-white border-2 border-pink rounded-sm'
+                />
+                <button
+                    className='p-2.5 w-20 bg-light-pink text-white rounded-md cursor-pointer transition-all hover:bg-hover-pink'
+                >Add</button>
+            </section>
+        </header>
+    )
+}
